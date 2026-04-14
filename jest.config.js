ca28e2895b52eab35770d5c8ab8fb3b775534f86
Axl Tan <axllltan@gmail.com>
2026-04-14 01:21:37 -0700

    Installed Jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
 // jest.config.js
-const { TestEnvironment } = require('jest-environment-node')
 const nextJest = require('next/jest')
 
 // next/jest sets up the transform and module resolution that matches Next.js
 const createJestConfig = nextJest( {dir: './' })
 
 module.exports = createJestConfig({
-    TestEnvironment: 'node',            // jsdom for Reach components, 'node' for API/lib code
+    testEnvironment: 'node',            // jsdom for Reach components, 'node' for API/lib code
     setupFilesAfterFramework: [],       // add global setup later if needed
-    moduleNameMappers: {
+    moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/$1',     // matches tsconfig path alias
     },
 })
